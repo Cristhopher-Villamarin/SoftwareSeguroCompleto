f29d97ce1aa786c0f0b3a289d62202cecb8fabbd
SebastianTorres12 <105263296+SebastianTorres12@users.noreply.github.com>
2025-03-09 14:53:26 -0500

    asdasd

--- frontend/src/ApiConfig.js
+++ frontend/src/ApiConfig.js
@@ -1,6 +1,6 @@
 // src/config/apiConfig.js
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "http://52.146.36.169:8080/api";
 
 // Define base URLs for each resource
 const BASE_URLS = {
